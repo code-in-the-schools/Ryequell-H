c150f3d2529107f387fd78475a4b185157706601
CodingQuell <ryequellhunter@gmail.com>
2020-07-14 17:26:29 +0000

    alot

---
+++ main.py
@@ -0,0 +1,31 @@
+print("Hello World")
+
+name= "My name is Ryequell Hunter"
+
+print(name)
+
+song= "Song: Rags2Riches"
+
+print(song)
+
+vacation= "Vacation: Miami"
+
+print(vacation)
+
+hobby= "Hobby: Cooking"
+
+print(hobby)
+
+date= "Date: 7/14/2020"
+
+print(date)
+
+alpha= "I love eatting..."
+zoo= "I love working..."
+
+a= "fruit"
+b= "at youth works"
+
+print(alpha + a)
+
+print(zoo + b)
